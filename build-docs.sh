f55287cd0103a49345b3e3988458fe4716de2258
Andrey Antukh <niwi@niwi.be>
2014-03-08 18:37:39 +0100

    Add build docs script.

---
+++ build-docs.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+(cd doc; make)
+cp -vr doc/index.html /tmp/index.html;
+cp -vr doc/static /tmp/static
+git checkout gh-pages;
+rm -rf *
+mv -fv /tmp/index.html .
+mv -fv /tmp/static .
+git add --all index.html
+git add --all static
+git commit -a -m "Update doc"
