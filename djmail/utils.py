7cf7ae49964c4343812ee6a49a2d25abf307f55e
David Fischer <david.fischer.ch@gmail.com>
2015-11-06 10:40:41 +0100

    One less line

--- djmail/utils.py
+++ djmail/utils.py
@@ -9,8 +9,7 @@ from django.utils.encoding import force_bytes
 
 
 def deserialize_email_message(data):
-    raw_pickle_data = base64.b64decode(force_bytes(data))
-    return pickle.loads(raw_pickle_data)
+    return pickle.loads(base64.b64decode(force_bytes(data)))
 
 
 def serialize_email_message(email_message):
