bcad968ed236a93ecb356d7b3d94dda30f99fc2f
Mathieu Hinderyckx <mhindery@Mathieus-MacBook-Pro.local>
2016-11-16 22:30:15 +0100

    - Fixed celery upgrade + working tests - Added manage.py and re-organized files/folders - Changed testing setup to regular 'python manage.py test' - Deleted old Django versions from Travis.yml testing config

--- djmail/tasks.py
+++ djmail/tasks.py
@@ -6,6 +6,16 @@ from celery import shared_task
 from . import core
 from . import utils
 
+from celery import Celery
+
+app = Celery('djmail')
+
+# Using a string here means the worker don't have to serialize
+# the configuration object to child processes.
+# - namespace='CELERY' means all celery-related configuration keys
+#   should have a `CELERY_` prefix.
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
 
 @shared_task
 def send_messages(messages):
