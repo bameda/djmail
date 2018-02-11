1d987fb4c48e801fabcefdf2dc743d323555b87f
Joris de Ruiter <jorisnachtwacht@gmail.com>
2018-02-11 02:47:52 +0100

    Update admin.py - Show HTML content

--- djmail/admin.py
+++ djmail/admin.py
@@ -3,13 +3,22 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 from .models import Message
 
 
 @admin.register(Message)
 class MessageAdmin(admin.ModelAdmin):
-    list_display = ['uuid', 'from_email', 'to_email', 'status', 'priority', 'created_at', 'sent_at', 'retry_count']
-    list_filter = ['status', 'priority', 'created_at', 'sent_at', 'retry_count']
-    search_fields = ['from_email', 'to_email', 'subject', 'uuid']
+    list_display = ['subject', 'from_email', 'to_email', 'status', 'created_at', 'sent_at']
+    list_filter = ['status', 'created_at', 'sent_at']
+    search_fields = ['subject', 'from_email', 'to_email']
     date_hierarchy = 'created_at'
+    readonly_fields = ['uuid', 'subject', 'status', 'from_email', 'to_email', 'body_text', 'body_html', 'body_html_show', 'priority', 'retry_count', 'created_at', 'sent_at', 'exception']
+    
+    fields = ['subject', 'status', 'from_email', 'to_email', 'body_text', 'body_html_show', 'created_at', 'sent_at']
+    
+    def body_html_show(self, instance):
+        return mark_safe(instance.body_html)
+    body_html_show.allow_tags = True
+    body_html_show.short_description = "Body html"
